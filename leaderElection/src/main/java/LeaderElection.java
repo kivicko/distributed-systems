dced5f3bc691127ddfbbd6c29717d93504b7b67d
kivilcim.eray <kivilcim.eray@gmail.com>
2020-12-28 07:17:03 +0300

    zooKeeper watchers implemented.

--- leaderElection/src/main/java/LeaderElection.java
+++ leaderElection/src/main/java/LeaderElection.java
@@ -77,6 +77,7 @@ public class  LeaderElection implements Watcher {
                         zooKeeper.notify();
                     }
                 }
+                break;
         }
     }
 }
---
+++ leaderElection/src/main/java/WatchersDemo.java
@@ -0,0 +1,86 @@
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.IOException;
+import java.util.List;
+
+public class WatchersDemo implements Watcher {
+
+    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private static final int SESSION_TIMEOUT = 3000;
+    private static final String TARGET_ZNODE = "/target_znode";
+    private ZooKeeper zooKeeper;
+
+    public static void main(String[] args) throws Exception {
+        WatchersDemo watchersDemo = new WatchersDemo();
+        watchersDemo.connectToZookeeper();
+        watchersDemo.watchTargetZnode();
+        watchersDemo.run();
+        watchersDemo.close();
+    }
+
+    private void close() throws InterruptedException {
+        this.zooKeeper.close();
+    }
+
+    public void run() throws InterruptedException {
+        synchronized (zooKeeper) {
+            zooKeeper.wait();
+        }
+    }
+
+    public void connectToZookeeper() throws IOException {
+        this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+    }
+
+    public void watchTargetZnode() throws KeeperException, InterruptedException {
+        Stat stat = zooKeeper.exists(TARGET_ZNODE, this);
+        if (stat == null) {
+            return;
+        }
+
+        byte[] data = zooKeeper.getData(TARGET_ZNODE, this, stat);
+        List<String> children = zooKeeper.getChildren(TARGET_ZNODE, this);
+
+        System.out.println("Data : " + new String(data) + " , childern : " + children);
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        switch (event.getType()) {
+            case None:
+                if (event.getState() == Event.KeeperState.SyncConnected) {
+                    System.out.println("Successfully connected to ZooKeeper.");
+                } else {
+                    synchronized (zooKeeper) {
+                        System.out.println("Disconnected from zooKeeper.");
+                        zooKeeper.notify();
+                    }
+                }
+                break;
+            case NodeDeleted:
+                System.out.println(TARGET_ZNODE + " was deleted.");
+                break;
+            case NodeCreated:
+                System.out.println(TARGET_ZNODE + " was created.");
+                break;
+            case NodeDataChanged:
+                System.out.println(TARGET_ZNODE + " data changed.");
+                break;
+            case NodeChildrenChanged:
+                System.out.println(TARGET_ZNODE + " node children changed.");
+                break;
+        }
+
+        try {
+            watchTargetZnode();
+        } catch (KeeperException ke) {
+
+        } catch (InterruptedException ie) {
+
+        }
+    }
+}
